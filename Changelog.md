cc1a4184489fed6b9dea0d8c599879bb148b8184
Roberto Blázquez <elroby444@gmail.com>
2024-03-18 12:12:38 +0100

    1.0.5

---
+++ Changelog.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## 1.0.5
+
+- Fix program crash when evaluating script
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 allprojects {
     group = "io.github.xbaank.LeagueProxy"
-    version = "1.0.3"
+    version = "1.0.5"
 }
 
 plugins {
